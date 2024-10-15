3f7ef66cd079be7a24f8da18bc4119909ae12a6a
cmarkos <xristosssmarkosss@gmail.com>
2024-10-15 22:10:54 +0300

    semifinal

--- main.go
+++ main.go
@@ -23,11 +23,8 @@ func main() {
 	// Split the lines of the file into a slice
 	lines := strings.Split(string(file), "\n")
 
-	// Capture the input from the user
-	inputText := os.Args[1]
-
 	// Split the input string into lines, based on literal newline characters
-	sepText := strings.Split(inputText, "\\n") // Handling literal "\n" from the input
+	sepText := strings.Split(os.Args[1], "\\n") // Handling literal "\n" from the input
 
 	// Print the ASCII Art based on the input
 	printAsciiArtRecursive(sepText, lines)
@@ -72,28 +69,3 @@ func printSentenceAsciiRecursive(word string, textFile []string, h int) {
 	// Recursive call for the next line height
 	printSentenceAsciiRecursive(word, textFile, h+1)
 }
-
-/*func printAsciiArt(sentences []string, textFile []string) {
-	// Loop through each sentence (split by '\n')
-	for i, word := range sentences {
-		if word == "" {
-			// If the sentence is empty (i.e., there was a '\n'), print an empty line
-			if i != 0 {
-				fmt.Println()
-			}
-			continue
-		}
-
-		for h := 1; h < 9; h++ {
-			for i := 0; i < len(word); i++ {
-				for lineIndex, line := range textFile {
-					if lineIndex == (int(word[i])-32)*9+h {
-						// Print the line corresponding to the current character and line number
-						fmt.Print(line)
-					}
-				}
-			}
-			fmt.Println()
-		}
-	}
-}*/
