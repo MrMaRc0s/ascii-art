10b9c30bc2fec612e981cc854377935a310e607c
cmarkos <xristosssmarkosss@gmail.com>
2024-10-15 21:54:14 +0300

    recursive2

--- main.go
+++ main.go
@@ -54,3 +54,28 @@ func printSentenceAscii(word string, textFile []string, h int) {
 	// Recursive call for the next line height
 	printSentenceAscii(word, textFile, h+1)
 }
+
+/*func printAsciiArt(sentences []string, textFile []string) {
+	// Loop through each sentence (split by '\n')
+	for i, word := range sentences {
+		if word == "" {
+			// If the sentence is empty (i.e., there was a '\n'), print an empty line
+			if i != 0 {
+				fmt.Println()
+			}
+			continue
+		}
+
+		for h := 1; h < 9; h++ {
+			for i := 0; i < len(word); i++ {
+				for lineIndex, line := range textFile {
+					if lineIndex == (int(word[i])-32)*9+h {
+						// Print the line corresponding to the current character and line number
+						fmt.Print(line)
+					}
+				}
+			}
+			fmt.Println()
+		}
+	}
+}*/
